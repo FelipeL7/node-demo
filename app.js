2a820396e697a08191cc357f99da9f87eb2ed0cd
FelipeL7 <felipelseven@gmail.com>
2020-09-14 03:29:40 -0300

    08-07- Add Persistance to Genre API

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const logger = require("./middleware/logger");
 const genres = require("./routes/genres");
 const home = require("./routes/home");
 const app = express();
+
+mongoose
+  // .connect("mongodb://localhost:27017/node-app.node-vidly", {
+  //   useNewUrlParser: true,
+  // })
+  .connect("mongodb://localhost/node-app", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((error) => console.error("Could not connect to MongoDB"));
 
 app.use(express.json());
 app.use(logger);
--- routes/genres.js
+++ routes/genres.js
@@ -1,50 +1,93 @@
+const joi = require("joi");
 const express = require("express");
+const mongoose = require("mongoose");
+const Joi = require("joi");
 // const app = express() It does not work when we separate the files
 const router = express.Router();
 
-const genres = [
-  { id: 1, name: "Action" },
-  { id: 2, name: "Horror" },
-  { id: 3, name: "Romance" },
-];
+const Genre = mongoose.model(
+  "Genre",
+  new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 50,
+    },
+  })
+);
 
-router.get("/", (req, res) => {
+// const genres = [
+//   { id: 1, name: "Action" },
+//   { id: 2, name: "Horror" },
+//   { id: 3, name: "Romance" },
+// ];
+
+router.get("/", async (req, res) => {
+  const genres = await Genre.find().sort("name");
   res.send(genres);
 });
 
-router.post("/", (req, res) => {
-  const genre = {
-    id: genres.length + 1,
-    name: req.body.name,
-  };
-  if (!genre || !genre.name)
-    return res.status(400).send("Invalid genre, try it again.");
-  genres.push(genre);
+router.post("/", async (req, res) => {
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let genre = new Genre({ name: req.body.name });
+
+  genre = await genre.save();
+
   res.send(genre);
 });
 
-router.put("/:id", (req, res) => {
-  const genre = genres.find((c) => c.id === parseInt(req.params.id));
+router.put("/:id", async (req, res) => {
+  try {
+    const { error } = validateGenre(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    // Update first approach
+    const genre = await Genre.findByIdAndUpdate(
+      req.params.id,
+      { name: req.body.name },
+      // Get the updated object
+      { new: true, useFindAndModify: false }
+    );
+
+    res.send(genre);
+  } catch (ex) {
+    return res.status(404).send("The genre with the given ID was not found.");
+  }
+
+  // if (!genre)
+  //   return res.status(404).send("The genre with the given ID was not found.");
+
+  // res.send(genre);
+});
+
+router.delete("/:id", async (req, res) => {
+  const genre = await Genre.findByIdAndRemove(req.params.id, {
+    useFindAndModify: false,
+  });
+
   if (!genre)
     return res.status(404).send("The genre with the given ID was not found.");
-  genre.name = req.body.name;
+
   res.send(genre);
 });
 
-router.delete("/:id", (req, res) => {
-  const genre = genres.find((c) => c.id === parseInt(req.params.id));
-  if (!genre)
-    return res.status(404).send("The genre with the given ID was not found.");
-  const index = genres.indexOf(genre);
-  genres.splice(index, 1);
-  res.send(genre);
-});
+router.get("/:id", async (req, res) => {
+  const genre = await Genre.findById(req.params.id);
 
-router.get("/:id", (req, res) => {
-  const genre = genres.find((c) => c.id === parseInt(req.params.id));
   if (!genre)
     res.status(404).send("The genre with the given ID was not found.");
+
   res.send(genre);
 });
+
+function validateGenre(genre) {
+  const schema = joi.object({
+    name: Joi.string().required().min(3),
+  });
+
+  return schema.validate(genre);
+}
 
 module.exports = router;
