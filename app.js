82f90ab156f1a694a1f5b60546521997399db3a0
FelipeL7 <felipelseven@gmail.com>
2020-09-15 23:10:06 -0300

    10-05- Hashing passwords

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ Joi.objectId = require("joi-objectid")(Joi);
 const express = require("express");
 const mongoose = require("mongoose");
 
+const users = require("./routes/users");
 const genres = require("./routes/genres");
 const movies = require("./routes/movies");
-const customers = require("./routes/customers");
-const logger = require("./middleware/logger");
 const rentals = require("./routes/rentals");
+const logger = require("./middleware/logger");
+const customers = require("./routes/customers");
 
 const app = express();
 
@@ -15,6 +16,7 @@ mongoose
   .connect("mongodb://localhost/node-app", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
   })
   .then(() => console.log("Connected to MongoDB..."))
   .catch((error) => console.error("Could not connect to MongoDB"));
@@ -25,5 +27,6 @@ app.use("/api/genres", genres);
 app.use("/api/movies", movies);
 app.use("/api/customers", customers);
 app.use("/api/rentals", rentals);
+app.use("/api/users", users);
 
 app.listen(3000, () => console.log("Listening on port 3000"));
